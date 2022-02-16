15c9805e8dc5fb568e99f60ef94cb821dc6913bc
Ayon <ayonjodder177@gmail.com>
2022-02-16 16:03:10 +0600

    Hero section added

---
+++ script.js
@@ -0,0 +1,7 @@
+// Auto type animation
+let typed = new Typed(".auto-type", {
+    strings: ["Ayon Jodder", "UI Designer", "Web Developer"],
+    typeSpeed: 150,
+    backSpeed: 100,
+    loop: true
+})
